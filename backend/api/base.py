4ad8da32f07c439f5c0fa2f374ef6fafcc13b314
Dmitry Tolochko <DimaTol36@gmail.com>
2025-11-30 21:20:53 +0500

    Now its working

--- backend/api/base.py
+++ backend/api/base.py
@@ -3,32 +3,32 @@ from starlette.exceptions import HTTPException
 
 
 class BadRequestException(HTTPException):
-    def __init__(self, detail: str | None = None) -> None:
+    def __init__(self, detail):
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
 
 
 class ForbiddenException(HTTPException):
-    def __init__(self, detail: str | None = None) -> None:
+    def __init__(self, detail):
         super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail=detail)
 
 
 class NotFoundException(HTTPException):
     def __init__(
         self,
-        detail: str | None = None,
-        obj: str | None = None,
-    ) -> None:
+        detail,
+        obj,
+    ):
         detail = detail or f"{obj} not found"
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
 
 
 class ConflictException(HTTPException):
-    def __init__(self, detail: str | None = None) -> None:
+    def __init__(self, detail):
         super().__init__(status_code=status.HTTP_409_CONFLICT, detail=detail)
 
 
 class FailedDependencyException(HTTPException):
-    def __init__(self, detail: str | None = None) -> None:
+    def __init__(self, detail):
         super().__init__(
             status_code=status.HTTP_424_FAILED_DEPENDENCY,
             detail=detail,
